db52695b332bc6dea08237dac99f75493249b333
codehia <ach4825@gmail.com>
2023-08-08 00:17:45 +0530

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,11 +1,29 @@
 {
-  description = "A very basic flake";
-
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  description = "My basic flake";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flakeUtils.url = "github:numtide/flake-utils";
+    homeManager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
+
+  outputs = { nixpkgs, unstable, homeManager, flakeUtils, ... }:
+    flakeUtils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        unstablePkgs = unstable.legacyPackages.${system};
+      in
+      {
+        legacyPackages.homeConfigurations."deus" = homeManager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ ./home.nix ./desktop.nix ];
+          extraSpecialArgs = {
+            unstable = unstablePkgs;
+          };
+        };
+      }
+    );
 }
