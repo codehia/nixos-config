6e078df14f7f7a851ea9d4f19e578d4126ab3039
Soumyaranjan Acharya <ach4825@gmail.com>
2025-08-10 00:48:20 +0530

    Add waybar and some fonts

---
+++ waybar/default.nix
@@ -0,0 +1,14 @@
+{lib, ...}: let
+  configJsonData = lib.importJSON ./waybar.json;
+  configCssData = ./waybar.css;
+in {
+  programs.waybar = {
+    enable = true;
+    settings = [configJsonData];
+    style = lib.mkForce configCssData;
+    systemd = {
+      enable = true;
+      target = "hyprland-session.target";
+    };
+  };
+}
