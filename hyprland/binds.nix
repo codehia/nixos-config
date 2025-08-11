3c6e4313cb1434d67d21a8da8f731a27f0a31e55
Soumyaranjan Acharya <ach4825@gmail.com>
2025-08-11 10:46:33 +0530

    Persistent workspaces

--- hyprland/binds.nix
+++ hyprland/binds.nix
@@ -1,12 +1,4 @@
-{host, ...}:
-# let
-#   inherit
-#     (import ../../../hosts/${host}/variables.nix)
-#     browser
-#     terminal
-#     ;
-# in
-{
+{...}: {
   wayland.windowManager.hyprland.settings = {
     bind = [
       "$modifier, Q, exec, ghostty"
@@ -62,28 +54,21 @@
       "$modifier,l,movefocus,r"
       "$modifier,k,movefocus,u"
       "$modifier,j,movefocus,d"
+
       "$modifier,1,workspace,1"
       "$modifier,2,workspace,2"
       "$modifier,3,workspace,3"
       "$modifier,4,workspace,4"
       "$modifier,5,workspace,5"
-      "$modifier,6,workspace,6"
-      "$modifier,7,workspace,7"
-      "$modifier,8,workspace,8"
-      "$modifier,9,workspace,9"
-      "$modifier,0,workspace,10"
-      "$modifier SHIFT,SPACE,movetoworkspace,special"
-      "$modifier,SPACE,togglespecialworkspace"
       "$modifier SHIFT,1,movetoworkspace,1"
       "$modifier SHIFT,2,movetoworkspace,2"
       "$modifier SHIFT,3,movetoworkspace,3"
       "$modifier SHIFT,4,movetoworkspace,4"
       "$modifier SHIFT,5,movetoworkspace,5"
-      "$modifier SHIFT,6,movetoworkspace,6"
-      "$modifier SHIFT,7,movetoworkspace,7"
-      "$modifier SHIFT,8,movetoworkspace,8"
-      "$modifier SHIFT,9,movetoworkspace,9"
-      "$modifier SHIFT,0,movetoworkspace,10"
+
+      "$modifier SHIFT,SPACE,movetoworkspace,special"
+      "$modifier,SPACE,togglespecialworkspace"
+
       "$modifier CONTROL,right,workspace,e+1"
       "$modifier CONTROL,left,workspace,e-1"
       "$modifier,mouse_down,workspace, e+1"
