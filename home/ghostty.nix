014125cd9d1fff05181074c2c5f72b30d23f630d
Soumyaranjan Acharya <ach4825@gmail.com>
2025-08-13 18:23:45 +0530

    PERSONAL: refactors and configurations for kitty and ghostty

---
+++ home/ghostty.nix
@@ -0,0 +1,19 @@
+_: {
+  programs.ghostty = {
+    enable = true;
+    clearDefaultKeybinds = true;
+    enableFishIntegration = true;
+    installBatSyntax = true;
+    settings = {
+      font-family = "JetBrainsMono Nerd Font Mono";
+      font-size = 14;
+      adjust-cell-height = "20%";
+      cursor-style = "block";
+      cursor-style-blink = true;
+      shell-integration-features = "no-cursor";
+      theme = "catppuccin-mocha";
+      window-padding-balance = true;
+      window-decoration = false;
+    };
+  };
+}
---
+++ home/kitty.nix
@@ -0,0 +1,22 @@
+{lib, ...}: {
+  programs.kitty = {
+    enable = true;
+    enableGitIntegration = true;
+    font = {
+      name = "JetBrainsMono Nerd Font Mono";
+      size = 14;
+    };
+    shellIntegration = {
+      enableFishIntegration = true;
+      mode = "no-rc no-cursor";
+    };
+    extraConfig = ''
+      cursor #cccccc
+      cursor_shape block
+      cursor_beam_thickness 1.5
+      cursor_blink_interval -1
+      cursor_stop_blinking_after 15.0
+    '';
+    themeFile = lib.mkForce "Catppuccin-Macchiato";
+  };
+}
