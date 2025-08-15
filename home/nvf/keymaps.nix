c45f928c8188257098ec67bebeb1f498eb6a3b08
Soumyaranjan Acharya <ach4825@gmail.com>
2025-08-16 00:44:31 +0530

    Add rofi, some stylix and tmux (broken)

--- home/nvf/keymaps.nix
+++ home/nvf/keymaps.nix
@@ -1,14 +1,54 @@
-{lib, ...}: let
-  inherit (lib.modules) mkIf;
-  inherit (lib.nvim.binds) mkKeymap;
-in {
+_: {
   programs.nvf.settings.vim = {
     keymaps = [
-      (mkKeymap "n" "<Esc>" "<cmd>nohlsearch<CR>")
+      {
+        key = "<Esc>";
+        mode = ["n"];
+        action = ":nohl<CR>";
+        desc = "Clear search highlights";
+      }
+      {
+        key = "<leader>ff";
+        mode = ["n"];
+        action = "<cmd>Telescope find_files<cr>";
+        desc = "Search files by name";
+      }
+      {
+        key = "<leader>lg";
+        mode = ["n"];
+        action = "<cmd>Telescope live_grep<cr>";
+        desc = "Search files by contents";
+      }
+      {
+        key = "<leader>fe";
+        mode = ["n"];
+        action = "<cmd>Neotree toggle<cr>";
+        desc = "File browser toggle";
+      }
+      {
+        key = "<C-h>";
+        mode = ["i"];
+        action = "<Left>";
+        desc = "Move left in insert mode";
+      }
+      {
+        key = "<C-j>";
+        mode = ["i"];
+        action = "<Down>";
+        desc = "Move down in insert mode";
+      }
+      {
+        key = "<C-k>";
+        mode = ["i"];
+        action = "<Up>";
+        desc = "Move up in insert mode";
+      }
+      {
+        key = "<C-l>";
+        mode = ["i"];
+        action = "<Right>";
+        desc = "Move right in insert mode";
+      }
     ];
-    # mappings = {
-    #   workspaceDiagnostics = mkMappingOption "Workspace diagnostics [Trouble]" "<leader>lwd";
-    #   hoHlSearch = mkMappingOption "Clear the highlight from search results" "<>"
-    # };
   };
 }
