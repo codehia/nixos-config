91382e7e80fcf0869cc74e3489862caa838d3463
Soumyaranjan Acharya <ach4825@gmail.com>
2025-08-19 00:33:04 +0530

    PERSONAL: minor Changes

--- home/default.nix
+++ home/default.nix
@@ -14,21 +14,24 @@
     ./tmux.nix
   ];
 
-  services.hyprsunset = {
-    enable = true;
-    transitions = {
-      sunrise = {
-        calendar = "*-*-* 06:00:00";
-        requests = [
-          ["temperature" "6500"]
-          ["gamma 100"]
-        ];
-      };
-      sunset = {
-        calendar = "*-*-* 19:00:00";
-        requests = [
-          ["temperature" "3000"]
-        ];
+  services = {
+    dunst.enable = true;
+    hyprsunset = {
+      enable = true;
+      transitions = {
+        sunrise = {
+          calendar = "*-*-* 06:00:00";
+          requests = [
+            ["temperature" "6500"]
+            ["gamma 100"]
+          ];
+        };
+        sunset = {
+          calendar = "*-*-* 19:00:00";
+          requests = [
+            ["temperature" "3000"]
+          ];
+        };
       };
     };
   };
