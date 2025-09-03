692fff2f41ecf299817992321565a7026e2018b1
Soumyaranjan Acharya <ach4825@gmail.com>
2025-09-04 00:39:46 +0530

    Multimachine

--- hosts/nixos-vm/hardware-configuration.nix
+++ hosts/nixos-vm/hardware-configuration.nix
@@ -33,6 +33,12 @@
   # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode =
-    lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware = {
+    cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+    graphics.enable = true;
+    amdgpu = {
+      initrd.enable = true;
+      amdvlk.enable = true;
+    };
+  };
 }
