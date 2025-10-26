590c76002e5629b199ce936cbcc7c0b19c44ef79
Soumyaranjan Acharya <ach4825@gmail.com>
2025-10-26 17:27:34 +0530

    PERSONAL: updates for waybar, kanshi and some deprecated keys

--- hosts/common/home/default.nix
+++ hosts/common/home/default.nix
@@ -17,53 +17,66 @@
     kanshi = {
       enable = true;
       systemdTarget = "hyprland-session.target";
-      profiles = {
-        undocked = {
-          outputs = [{
-            criteria = "eDP-1";
-            mode = "1920x1080@60.03300";
-            scale = 1.0;
-            position = "0,0";
-            status = "enable";
-          }];
-        };
-        azanDocked = {
-          outputs = [
-            {
-              criteria = "BNQ BenQ GW2480 BCP0111201Q";
-              mode = "1920x1080@60.00Hz";
-            }
-            {
+      settings = [
+        {
+          profile = {
+            name = "undocked";
+            outputs = [{
               criteria = "eDP-1";
-              status = "disable";
-            }
-          ];
-        };
-        miniDocked = {
-          outputs = [
-            {
-              criteria = "Samsung Electric Company LF24T35 HNAR101094";
-              mode = "1920x1080@74.97300";
-            }
-            {
-              criteria = "eDP-1";
-              status = "disable";
-            }
-          ];
-        };
-        docked = {
-          outputs = [
-            {
-              criteria = "LG Electronics LG HDR WQHD 0x0001991D";
-              mode = "3440x1440@75.05Hz";
-            }
-            {
-              criteria = "eDP-1";
-              status = "disable";
-            }
-          ];
-        };
-      };
+              mode = "1920x1080@60.03300";
+              scale = 1.0;
+              position = "0,0";
+              status = "enable";
+            }];
+          };
+        }
+        {
+          profile = {
+            name = "azanDocked";
+            outputs = [
+              {
+                criteria = "BNQ BenQ GW2480 BCP0111201Q";
+                mode = "1920x1080@60.00Hz";
+              }
+              {
+                criteria = "eDP-1";
+                status = "disable";
+              }
+            ];
+          };
+        }
+        {
+          profile = {
+            name = "miniDocked";
+            outputs = [
+              {
+                criteria = "Samsung Electric Company LF24T35 HNAR101094";
+                mode = "1920x1080@74.97300";
+              }
+              {
+                criteria = "eDP-1";
+                status = "disable";
+              }
+            ];
+          };
+        }
+        {
+          profile = {
+            name = "docked";
+            outputs = [
+              {
+                criteria = "LG Electronics LG HDR WQHD 0x0001991D";
+                mode = "3440x1440@75.05Hz";
+              }
+              {
+                criteria = "eDP-1";
+                status = "disable";
+              }
+            ];
+          };
+
+        }
+      ];
     };
     dunst.enable = true;
     hyprsunset = {
@@ -188,6 +201,8 @@
       libglvnd
       libglibutil
       zoom-us
+
+      libreoffice-still
     ];
     stateVersion = "25.05";
   };
