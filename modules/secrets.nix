2ed6c77f40bdcc5731ebb868a86492c46b5af431
codehia <dev@sacharya.dev>
2026-03-03 01:02:55 +0530

    THINKPAD: split the rclone and ssh secrets

--- modules/secrets.nix
+++ modules/secrets.nix
@@ -1,48 +1,27 @@
-{inputs, ...}: {
+{ inputs, ... }:
+{
   flake-file.inputs.sops-nix = {
     url = "github:Mic92/sops-nix";
     inputs.nixpkgs.follows = "nixpkgs";
   };
 
   den.aspects.secrets = {
-    nixos = {...}: {
-      imports = [inputs.sops-nix.nixosModules.sops];
-
-      sops = {
-        defaultSopsFile = ../secrets/thinkpad.yaml;
-        age.keyFile = "/var/lib/sops/age/keys.txt";
-
-        secrets = {
-          ssh_host_ed25519_key = {
-            path = "/etc/ssh/ssh_host_ed25519_key";
-            owner = "root";
-            group = "root";
-            mode = "0600";
-          };
-          ssh_host_rsa_key = {
-            path = "/etc/ssh/ssh_host_rsa_key";
-            owner = "root";
-            group = "root";
-            mode = "0600";
-          };
+    nixos =
+      { ... }:
+      {
+        imports = [ inputs.sops-nix.nixosModules.sops ];
+        sops = {
+          age.keyFile = "/var/lib/sops/age/keys.txt";
         };
       };
 
-      # Prevent openssh from auto-generating keys (we provide them via sops)
-      services.openssh.hostKeys = [];
-    };
-
-    homeManager = {config, ...}: {
-      imports = [inputs.sops-nix.homeManagerModules.sops];
-
-      sops = {
-        defaultSopsFile = ../secrets/common.yaml;
-        age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
-
-        secrets.rclone_conf = {
-          path = "${config.home.homeDirectory}/.config/rclone/rclone.conf";
+    homeManager =
+      { config, ... }:
+      {
+        imports = [ inputs.sops-nix.homeManagerModules.sops ];
+        sops = {
+          age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
         };
       };
-    };
   };
 }
