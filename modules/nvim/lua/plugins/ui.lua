6218c8cd70dde8c87de1ab6d79db46713136e5f5
codehia <dev@sacharya.dev>
2026-03-03 22:34:32 +0530

    WORKSTATION: Add hardtime-nvim

--- modules/nvim/lua/plugins/ui.lua
+++ modules/nvim/lua/plugins/ui.lua
@@ -238,7 +238,7 @@ return {
     cmd = { 'StartupTime' },
     before = function()
       vim.g.startuptime_event_width = 0
-      vim.g.startuptime_tries = 10
+      vim.g.startuptime_tries = 0
     end,
   },
 
