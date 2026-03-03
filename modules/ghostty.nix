d74ec028f6d47e98194e8bf5e515d35ee5db7931
codehia <dev@sacharya.dev>
2026-03-03 11:13:54 +0530

    WORKSTATION: minor miscellaneous changes
    1. add dms bindings to hyprland
    2. change the font config in ghostty
    3. change the window change keybindings in tmux

--- modules/ghostty.nix
+++ modules/ghostty.nix
@@ -1,22 +1,25 @@
-{...}: {
+{ ... }:
+{
   den.aspects.ghostty = {
-    homeManager = {pkgs, ...}: {
-      programs.ghostty = {
-        package = pkgs.unstable.ghostty;
-        enable = true;
-        enableFishIntegration = true;
-        installBatSyntax = true;
-        settings = {
-          font-family = "Iosevka Nerd Font Mono";
-          font-size = 13;
-          cursor-style = "block";
-          cursor-style-blink = true;
-          shell-integration-features = "no-cursor";
-          window-padding-balance = true;
-          window-decoration = false;
-          keybind = "ctrl+enter=unbind";
+    homeManager =
+      { pkgs, ... }:
+      {
+        programs.ghostty = {
+          package = pkgs.unstable.ghostty;
+          enable = true;
+          enableFishIntegration = true;
+          installBatSyntax = true;
+          settings = {
+            font-family = "Iosevka Nerd Font";
+            font-size = 14;
+            cursor-style = "block";
+            cursor-style-blink = true;
+            shell-integration-features = "no-cursor";
+            window-padding-balance = true;
+            window-decoration = false;
+            keybind = "ctrl+enter=unbind";
+          };
         };
       };
-    };
   };
 }
