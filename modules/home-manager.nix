35f25722da3a8dd1d3a4d6d121fd1f83b5f96e5e
codehia <dev@sacharya.dev>
2026-03-05 09:06:48 +0530

    PERSONAL: Fix the "homeManager" issue for user classes issue

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -2,12 +2,15 @@
 #
 # Home-manager is now enabled per-host via den.hosts.<system>.<hostname>.home-manager.enable = true.
 # User accounts are created automatically by den when users are declared in den.hosts.
-{ den, ... }:
+{ den, lib, ... }:
 {
   flake-file.inputs.home-manager = {
     url = "github:nix-community/home-manager/release-25.11";
     inputs.nixpkgs.follows = "nixpkgs";
   };
+
+  # Give all users the homeManager class by default so den imports the HM NixOS module.
+  den.base.user.classes = lib.mkDefault [ "homeManager" ];
 
   den.default = {
     nixos.home-manager.backupFileExtension = "hm-backup";
