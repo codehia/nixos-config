789ba614402231806f77f45fe03f976dcfde5030
codehia <dev@sacharya.dev>
2026-04-23 10:35:32 +0530

    THINKPAD: Remove rclone from thinkpad

--- modules/hosts/thinkpad/default.nix
+++ modules/hosts/thinkpad/default.nix
@@ -11,9 +11,7 @@
     wm = "swayfx";
     # Aspects added here are picked up by deus's extraAspectsSelector and included only on this host.
     # Useful when deus needs an aspect on some hosts but not all (e.g. work tools on a work laptop).
-    extraAspects = [
-      "rclone"
-    ];
+    extraAspects = [ ];
     nvimLanguages = [
       "lua"
       "nix"
