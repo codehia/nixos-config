043be34faba47729f063f1112022f5c09e3d1341
codehia <dev@sacharya.dev>
2026-04-23 10:17:18 +0530

    PERSONAL: Bunch of home cleaning settings.

--- modules/aspects/home-manager.nix
+++ modules/aspects/home-manager.nix
@@ -10,5 +10,31 @@
 
   den.default = {
     nixos.home-manager.backupFileExtension = "hm-backup";
+
+    homeManager =
+      { config, ... }:
+      {
+        xdg.enable = true;
+        xdg.userDirs = {
+          enable = true;
+          createDirectories = true;
+          music = "${config.home.homeDirectory}/Media/Music";
+        };
+        home.sessionVariables = {
+          CARGO_HOME = "$HOME/.local/share/cargo";
+          RUSTUP_HOME = "$HOME/.local/share/rustup";
+          BUN_INSTALL = "$HOME/.local/share/bun";
+          DOTNET_CLI_HOME = "$HOME/.local/share/dotnet";
+          IPYTHONDIR = "$HOME/.config/ipython";
+          LESSHISTFILE = "$HOME/.local/state/less/history";
+          NODE_REPL_HISTORY = "$HOME/.local/state/node_repl_history";
+          NPM_CONFIG_USERCONFIG = "$HOME/.config/npm/npmrc";
+          COOKIECUTTER_CONFIG = "$HOME/.config/cookiecutter/config.yaml";
+        };
+        home.file.".config/npm/npmrc".text = ''
+          cache=''${XDG_CACHE_HOME:-$HOME/.cache}/npm
+          prefix=''${XDG_DATA_HOME:-$HOME/.local/share}/npm
+        '';
+      };
   };
 }
