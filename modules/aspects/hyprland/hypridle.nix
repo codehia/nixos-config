52019be712455330c0e57d6e773c075a38a83fad
Soumyaranjan Acharya <>
2026-04-25 23:51:11 +0530

    WORKSTATION: Auto resume hyprsunset on wake up

--- modules/aspects/hyprland/hypridle.nix
+++ modules/aspects/hyprland/hypridle.nix
@@ -13,6 +13,7 @@
           settings = {
             general = {
               before_sleep_cmd = "${pkgs.playerctl}/bin/playerctl -a pause";
+              after_sleep_cmd = "${pkgs.systemd}/bin/systemctl --user restart hyprsunset.service";
               on_lock_cmd = "${pkgs.playerctl}/bin/playerctl -a pause";
             };
           };
