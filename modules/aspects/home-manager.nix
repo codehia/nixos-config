9484c7673edd83550221de1dfcd7c9a3dea9ff3e
codehia <dev@sacharya.dev>
2026-05-08 02:41:31 +0530

    THINKPAD: update and a little refactor

--- modules/aspects/home-manager.nix
+++ modules/aspects/home-manager.nix
@@ -14,27 +14,31 @@
     homeManager =
       { config, ... }:
       {
-        xdg.enable = true;
-        xdg.userDirs = {
+        xdg = {
           enable = true;
-          createDirectories = true;
-          music = "${config.home.homeDirectory}/Media/Music";
+          userDirs = {
+            enable = true;
+            createDirectories = true;
+            music = "${config.home.homeDirectory}/Media/Music";
+          };
         };
-        home.sessionVariables = {
-          CARGO_HOME = "$HOME/.local/share/cargo";
-          RUSTUP_HOME = "$HOME/.local/share/rustup";
-          BUN_INSTALL = "$HOME/.local/share/bun";
-          DOTNET_CLI_HOME = "$HOME/.local/share/dotnet";
-          IPYTHONDIR = "$HOME/.config/ipython";
-          LESSHISTFILE = "$HOME/.local/state/less/history";
-          NODE_REPL_HISTORY = "$HOME/.local/state/node_repl_history";
-          NPM_CONFIG_USERCONFIG = "$HOME/.config/npm/npmrc";
-          COOKIECUTTER_CONFIG = "$HOME/.config/cookiecutter/config.yaml";
+        home = {
+          sessionVariables = {
+            CARGO_HOME = "$HOME/.local/share/cargo";
+            RUSTUP_HOME = "$HOME/.local/share/rustup";
+            BUN_INSTALL = "$HOME/.local/share/bun";
+            DOTNET_CLI_HOME = "$HOME/.local/share/dotnet";
+            IPYTHONDIR = "$HOME/.config/ipython";
+            LESSHISTFILE = "$HOME/.local/state/less/history";
+            NODE_REPL_HISTORY = "$HOME/.local/state/node_repl_history";
+            NPM_CONFIG_USERCONFIG = "$HOME/.config/npm/npmrc";
+            COOKIECUTTER_CONFIG = "$HOME/.config/cookiecutter/config.yaml";
+          };
+          file.".config/npm/npmrc".text = ''
+            cache=''${XDG_CACHE_HOME:-$HOME/.cache}/npm
+            prefix=''${XDG_DATA_HOME:-$HOME/.local/share}/npm
+          '';
         };
-        home.file.".config/npm/npmrc".text = ''
-          cache=''${XDG_CACHE_HOME:-$HOME/.cache}/npm
-          prefix=''${XDG_DATA_HOME:-$HOME/.local/share}/npm
-        '';
       };
   };
 }
