d45ae3730db7cf2179120511a33106362302a6b1
codehia <dev@sacharya.dev>
2026-07-10 18:26:42 +0530

    THINKPAD: change thinkpad default wm to mango

--- modules/hosts/thinkpad/default.nix
+++ modules/hosts/thinkpad/default.nix
@@ -7,7 +7,7 @@
     isLaptop = true;
     nhCleanEnabled = true;
     greetdUser = "deus";
-    greetdSessionBin = "sway";
+    greetdSessionBin = "mango";
     # Aspects added here are picked up by deus's extraAspectsSelector and included only on this host.
     # Useful when deus needs an aspect on some hosts but not all (e.g. work tools on a work laptop).
     extraAspects = [ "rclone" ];
