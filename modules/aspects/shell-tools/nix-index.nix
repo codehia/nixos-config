0d8162d34a2a492fdf15d3efa960afcf1700c8ee
codehia <dev@sacharya.dev>
2026-07-25 13:34:33 +0530

    PERSONAL: Fix issue with nix-index

--- modules/aspects/shell-tools/nix-index.nix
+++ modules/aspects/shell-tools/nix-index.nix
@@ -1,8 +1,12 @@
+{ inputs, ... }:
 {
   den.aspects.shell-tools = {
-    homeManager.programs.nix-index = {
-      enable = true;
-      enableFishIntegration = true;
+    homeManager = {
+      imports = [ inputs.nix-index-database.homeModules.nix-index ];
+      programs.nix-index = {
+        enable = true;
+        enableFishIntegration = true;
+      };
     };
   };
 }
