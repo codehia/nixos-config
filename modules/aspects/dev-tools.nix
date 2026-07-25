df0fd52a56c61f04b1d78c53dd94d6a9dcf49a83
codehia <dev@sacharya.dev>
2026-07-25 20:15:27 +0530

    PERSONAL: Add docker to dev-tools

--- modules/aspects/dev-tools.nix
+++ modules/aspects/dev-tools.nix
@@ -1,7 +1,19 @@
 { den, ... }:
+let
+  dockerGroup =
+    { user, ... }:
+    {
+      nixos.users.users.${user.userName}.extraGroups = [ "docker" ];
+    };
+in
 {
   den.aspects.dev-tools = {
-    includes = [ (den._.unfree [ "httpie-desktop" ]) ];
+    includes = [
+      (den._.unfree [ "httpie-desktop" ])
+      dockerGroup
+    ];
+
+    nixos.virtualisation.docker.enable = true;
 
     homeManager =
       { pkgs, ... }:
@@ -10,6 +22,8 @@
           (with pkgs; [
             just
             devbox
+            cruft
+            cookiecutter
           ])
           ++ (with pkgs.unstable; [
             httpie-desktop
